873b61618de51330be49099b0b79c85376242212
Barak-S <barak.saidoff@gmail.com>
2020-12-14 22:43:15 -0500

    collapse home photo button resposive

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,5 +1,5 @@
 import { Steps } from 'antd';
-// import 'antd/dist/antd.css'
+import 'antd/dist/antd.css'
  import React from 'react';
  
  const Progress = () => {
--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Col, Row, Container } from 'react-bootstrap';
 
 export default function Footer() {
     return (
         <div className="ikon-footer" style={{ color: "#fff", backgroundColor: '#111', fontWeight: 600}}>
-            <p>© 2020, Ikon Weapons | All rights reserved</p>
+            <p style={{paddingTop: 8}}>© 2020, Ikon Weapons | All rights reserved</p>
         </div>
 
     )
