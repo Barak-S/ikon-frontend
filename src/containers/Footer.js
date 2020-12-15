c27a377f65b2f6272b8e5dbe0068e05924038c45
Barak-S <barak.saidoff@gmail.com>
2020-12-14 22:29:29 -0500

    remove row margin and responsive height

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -3,12 +3,8 @@ import { Col, Row, Container } from 'react-bootstrap';
 
 export default function Footer() {
     return (
-        <div style={{ textAling: 'center', color: "#fff", backgroundColor: '#111', paddingTop: 12, fontSize: 16, fontWeight: 600}}>
-                <Row>
-                    <Col>
-                        <p className="ikon-footer">© 2020, Ikon Weapons | All rights reserved | Terms Of Service | Privacy</p>
-                    </Col>
-                </Row>
+        <div className="ikon-footer" style={{ color: "#fff", backgroundColor: '#111', fontWeight: 600}}>
+            <p>© 2020, Ikon Weapons | All rights reserved</p>
         </div>
 
     )
