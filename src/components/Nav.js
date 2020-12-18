b332a15022138de1797b0163b787c7f3ce75acb2
Barak-S <barak.saidoff@gmail.com>
2020-12-18 09:58:24 -0500

    added sidebar nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import '../Nav.css';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
-import ShoppingCart from '../components/../images/cart.svg'
-import axios from 'axios'
+// import { Navbar, Nav } from 'react-bootstrap';
+// import ShoppingCart from '../components/../images/cart.svg'
+// import axios from 'axios'
 import logo from '../images/ikon-logo.png'
+
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { SidebarData } from './SidebarData';
+import { IconContext } from 'react-icons';
 
 const NavBar = (props) => {
 
     const [show, handleShow] = useState(false)
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => setSidebar(!sidebar);
 
     useEffect(()=>{
         window.addEventListener('scroll', ()=>{
@@ -24,16 +32,41 @@ const NavBar = (props) => {
     }, [])
 
     return (
-        <div className={`nav ${show && "nav-black"}`}>
-            <Link to="/">
-            <img 
-                src={logo}
-                className={`nav-logo ${show && "transition"}`}
-            >
-            </img>
+        <>
+        <IconContext.Provider value={{ color: '#fff' }}>
+            <div className={`nav ${show && "nav-black"}`}>
+            <Link to='#' className='menu-bars'>
+                <FaIcons.FaBars onClick={showSidebar} style={{ color: '#000' }} />
             </Link>
-        {/* <Link to="/login" className="nav-avatar">Log In</Link> */}
-    </div>
+            <Link to="/">
+                <img 
+                    src={logo}
+                    className={`nav-logo ${show && "transition"}`}
+                >
+                </img>
+            </Link>
+            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className='nav-menu-items' onClick={showSidebar}>
+                    <li className='navbar-toggle'>
+                    <Link to='#' className='menu-bars'>
+                        <AiIcons.AiOutlineClose />
+                    </Link>
+                    </li>
+                    {SidebarData.map((item, index) => {
+                    return (
+                        <li key={index} className={item.cName}>
+                        <Link to={item.path}>
+                            {item.icon}
+                            <span>{item.title}</span>
+                        </Link>
+                        </li>
+                    );
+                    })}
+                </ul>
+            </nav>            
+        </div>
+        </IconContext.Provider>
+    </>
     );
 };
 
