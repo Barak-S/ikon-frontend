ce6dfdbbc4a48c55f207ff5474147e3986800436
Barak-S <barak.saidoff@gmail.com>
2020-12-18 10:32:40 -0500

    added home and repair links to sidebar nav

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -2,43 +2,55 @@ import React from 'react';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
-import * as CgIcons from 'react-icons/cg';
+import * as GiIcons from 'react-icons/gi';
 
 export const SidebarData = [
-  {
-    title: 'Account',
-    path: '/account',
-    icon: <IoIcons.IoMdPerson />,
-    cName: 'nav-text'
-  },
-  {
-    title: 'Orders',
-    path: '/account/orders',
-    icon: <IoIcons.IoIosPaper />,
-    cName: 'nav-text'
-  },
-  {
-    title: 'Shop',
-    path: '/shop',
-    icon: <FaIcons.FaCartPlus />,
-    cName: 'nav-text'
-  },
-  {
-    title: 'Contact',
-    path: '/contact',
-    icon: <CgIcons.CgPhone />,
-    cName: 'nav-text'
-  },
-  {
-    title: 'Messages',
-    path: '/messages',
-    icon: <FaIcons.FaEnvelopeOpenText />,
-    cName: 'nav-text'
-  },
-  {
-    title: 'Support',
-    path: '/support',
-    icon: <IoIcons.IoMdHelpCircle />,
-    cName: 'nav-text'
-  }
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Account',
+        path: '/account',
+        icon: <IoIcons.IoMdPerson />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Orders',
+        path: '/account/orders',
+        icon: <IoIcons.IoIosPaper />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Shop',
+        path: '/shop',
+        icon: <FaIcons.FaShoppingCart />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Repair',
+        path: '/repair',
+        icon: <GiIcons.GiAutoRepair />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Contact',
+        path: '/contact',
+        icon: <AiIcons.AiOutlinePhone />,
+        cName: 'nav-text'
+    },
+    // {
+    //     title: 'Messages',
+    //     path: '/messages',
+    //     icon: <FaIcons.FaEnvelopeOpenText />,
+    //     cName: 'nav-text'
+    // },
+    {
+        title: 'Support',
+        path: '/support',
+        icon: <IoIcons.IoMdHelpCircle />,
+        cName: 'nav-text'
+    }
 ];
